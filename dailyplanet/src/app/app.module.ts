dbce989f258e53e3bf03684128f4790c4169661b
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-11-03 16:08:39 +0530

    CL-1.0.6: Created public components.

--- dailyplanet/src/app/app.module.ts
+++ dailyplanet/src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroComponent } from './components/public/hero/hero.component';
+import { AboutComponent } from './components/public/about/about.component';
+import { ContactComponent } from './components/public/contact/contact.component';
+import { PnfComponent } from './components/public/pnf/pnf.component';
+import { ReportComponent } from './components/public/report/report.component';
+import { DocumentationComponent } from './components/public/documentation/documentation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HeroComponent,
+    AboutComponent,
+    ContactComponent,
+    PnfComponent,
+    ReportComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule,
