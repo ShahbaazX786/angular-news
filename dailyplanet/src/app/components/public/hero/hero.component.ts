253087165b4a5aa8f8583348eafa0a0dd61a0e81
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-11-16 17:57:55 +0530

    CL-1.0.21: Used kevin Powell trick to limit the headlines to 3 lines and then ........ Also added a current date and time marquee tag. also added 1second delay in hover animation of card.

--- dailyplanet/src/app/components/public/hero/hero.component.ts
+++ dailyplanet/src/app/components/public/hero/hero.component.ts
@@ -4,21 +4,31 @@ import { NewsApiService } from './../../../services/news-api.service';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent implements OnInit {
+  constructor(private api: NewsApiService) {}
 
-  constructor(private api:NewsApiService) { }
-
-
-  HeadLinesArray:any= [];
+  timeNow: string = 'askldjfsjkld';
+  HeadLinesArray: any = [];
 
   ngOnInit(): void {
-    this.api.getTopHeadLines().subscribe((res)=>{
+    this.getTimeNow();
+    this.api.getTopHeadLines().subscribe((res) => {
       console.log(res.articles);
       this.HeadLinesArray = res.articles;
-
-    })
+    });
   }
 
+  getTimeNow() {
+    let time = new Date().toLocaleTimeString('en-US', {
+      hour12: true,
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+    let date = new Date().toLocaleDateString();
+    this.timeNow = " Today is " + date + " And now the time is: "+ " "+time;
+    console.log(this.timeNow);
+
+  }
 }
